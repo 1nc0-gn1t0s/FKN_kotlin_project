c450edc4ff26fefe4257283378a2e7fe06e974cc
1nc0-gn1t0s <katapaskova947@gmail.com>
2024-05-23 14:16:37 +0300

    committed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
+    implementation("androidx.test:runner:1.5.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+    implementation("org.apache.commons:commons-math3:3.6.1")
+    implementation("io.github.kakaocup:kakao:3.5.1")
 }
